5b2e1c08ef997d8712d0787c2f6667c711fb773f
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2014-01-27 15:02:47 +0100

    Added Receipt class

---
+++ Kassakvitto/Kassakvitto/Receipt.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kassakvitto
+{
+    public class Receipt
+    {
+        double _subtotal;
+        double DiscountRate
+        {
+            get;
+            set;
+        }
+        double MoneyOff
+        {
+            get;
+            set;
+        }
+        double Subtotal
+        {
+            get;
+            set;
+        }
+        double Total
+        {
+            get;
+            set;
+        }
+        void Calculate(double subtotal)
+        {
+
+        }
+        Receipt(double subtotal)
+        {
+
+        }
+    }
+}
